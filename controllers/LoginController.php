28277e4cabd52161dfe5c1b97b37f16fab7235f8
Ana Bernal <asakalieu@gmail.com>
2024-09-18 10:47:35 +0200

    primer commit

---
+++ controllers/LoginController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Controllers;
+
+class LoginController {
+
+    public static function login()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+        }
+    }
+
+    public static function logout()
+    {
+        
+    }
+
+    public static function crear()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+        }
+    }
+
+    public static function olvide()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+        }
+    }
+
+    public static function restablecer()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+        }
+    }
+
+    public static function mensaje()
+    {
+        
+    }
+
+    public static function confirmar()
+    {
+        
+    }
+}
